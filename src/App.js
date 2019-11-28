655ba6861f696533f340c4f23b1ba1bfa1a86461
King_Cheung <kingcheung0@gmail.com>
2019-11-28 00:23:39 +0000

    new project setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,117 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import { Workbook } from 'exceljs';
+import * as fs from 'file-saver';
+// import {LicenseManager} from "ag-grid-enterprise";
+// LicenseManager.setLicenseKey("your license key");
+
+
+
+
+
 
 function App() {
+  const [state, setState] = useState({
+    columnDefs: [{
+  headerName: "Make", field: "make", sortable:true
+},{
+  headerName: "Price", field: "price", sortable: true
+}],
+// autoGroupColumnDef: {
+//   headerName: "Model",
+//   field: "model",
+//   // cellRenderer:'agGroupCellRenderer',
+//   // cellRendererParams: {
+//   //   checkbox: true
+//   // }
+// },
+})
+
+const title = 'Car Sell Report';
+const header = ["Year", "Month", "Make", "Model", "Quantity", "Pct"]
+const data = [
+  [2007, 1, "Volkswagen ", "Volkswagen Passat", 1267, 10],
+  [2007, 1, "Toyota ", "Toyota Rav4", 819, 6.5],
+  [2007, 1, "Toyota ", "Toyota Avensis", 787, 6.2],
+  [2007, 1, "Volkswagen ", "Volkswagen Golf", 0, 5.7],
+  [2007, 1, "Toyota ", "Toyota Corolla", 691, 5.4],
+];
+
+let workbook = new Workbook();
+let worksheet = workbook.addWorksheet('Car Data');
+
+// Add new row
+let titleRow = worksheet.addRow([title]);
+// Set font, size and style in title row.
+titleRow.font = { name: 'Comic Sans MS', family: 4, size: 16, underline: 'double', bold: true };
+// Blank Row
+worksheet.addRow([]);
+//Add row with current date
+// let subTitleRow = worksheet.addRow(['Date : ' + this.datePipe.transform(new Date(), 'medium')]);
+
+//Add Header Row
+let headerRow = worksheet.addRow(header);
+
+// Cell Style : Fill and Border
+headerRow.eachCell((cell, number) => {
+  cell.fill = {
+    type: 'pattern',
+    pattern: 'solid',
+    fgColor: { argb: 'FFFFFF00' },
+    bgColor: { argb: 'FF0000FF' }
+  }
+  cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
+});
+
+// Add Data and Conditional Formatting
+data.forEach(d => {
+  let row = worksheet.addRow(d);
+  let qty = row.getCell(5);
+  let color = 'FF99FF99';
+  if (+qty.value < 500) {
+    color = 'FF9999'
+  }
+
+  qty.fill = {
+    type: 'pattern',
+    pattern: 'solid',
+    fgColor: { argb: color }
+  }
+}
+
+);
+
+
+const onClickButton = () => {
+  
+  workbook.xlsx.writeBuffer().then((data) => {
+    let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    fs.saveAs(blob, 'CarData.xlsx');
+  });
+
+}
+
+
+var dataFetched = false;
+useEffect(()=>{
+  if (state.rowData === undefined){
+  fetch('https://api.myjson.com/bins/ly7d1')
+.then(result => result.json())
+.then(rowData => {
+    setState({...state, rowData: rowData});
+    console.log(dataFetched)
+    dataFetched = true;
+    console.log(rowData);
+}
+)
+}
+})
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +119,29 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+	<h1>
+	  Testing this react app
+	</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          onClick = {onClickButton}
         >
           Learn React
         </a>
       </header>
+	<div
+        className="ag-theme-balham"
+        style={{
+        height: '500px',
+        width: '600px' }}
+      >
+        <AgGridReact
+          columnDefs={state.columnDefs}
+          rowData={state.rowData}>
+        </AgGridReact>
+      </div>
     </div>
   );
 }
