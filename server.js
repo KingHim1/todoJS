a67e3284cdb4bd664a3dd276f2f3428475bcf0b3
King_Cheung <kingcheung0@gmail.com>
2019-11-29 20:31:57 +0000

    again added extra api

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//connect to database
+var con = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: 'password',
+  database: "company"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+app.get("/api/pets", (req, res) => {
+  con.query("SELECT * FROM company.pet", function(err, result, fields) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+app.get("/api/", (req, res) => {
+    con.query("SELECT * FROM company.pet", function(err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+    });
+  });
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
